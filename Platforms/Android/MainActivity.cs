d70aba3998fe9020953606a86d922997a4855649
yusufemrekimsesiz <yusufemrekimsesiz@gmail.com>
2026-09-03 20:33:27 +0300

    Task hijacking icin runtime IsTaskRoot kontrolu, FileProvider path duzeltmesi dogrulandi

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -4,8 +4,18 @@ using Android.OS;
 
 namespace Equaly
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTask, TaskAffinity = "", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleInstance, TaskAffinity = "com.yusuf.equaly.maintask", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            if (!IsTaskRoot)
+            {
+                Finish();
+                return;
+            }
+        }
     }
 }
